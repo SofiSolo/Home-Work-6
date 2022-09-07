8125d5e5990957d6c8b5a34274e42e50f642fbc8
SofiSolo <sofa.galaxia@yandex.ru>
2022-09-08 00:16:10 +0300

    задание 2-4

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,12 @@ public class Main {
 
         for( int i=0; i <=10;i++){
             System.out.println(" Итерация цикла"+ i);}
+        for(int y=10;y>=1;y--){
+            System.out.println("Итерация цикла" + y);}
+
+        for(int e=0;e<=17 ;e++){
+            System.out.println( " Чётные числа " + e);}
+        for (int a =10;a>=-10;a--){
+            System.out.println(" Итерация по убыванию" +a);}
     }
 }
