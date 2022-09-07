9483a5f5b227f7771012b2cace110b444a266215
SofiSolo <sofa.galaxia@yandex.ru>
2022-09-07 23:55:53 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Циклы 1 часть!");
+
+
+        for( int i=0; i <=10;i++){
+            System.out.println(" Итерация цикла"+ i);}
+    }
+}
